b167bd692f8e9bb150dc6b9b8ade29fda7e4dcb3
Roccaldo <roccaldodigrisolo@gmail.com>
2024-02-28 14:59:18 +0100

    esx added

---
+++ src/main/java/co/develhope/springrepo1/SpringRepo1Application.java
@@ -0,0 +1,13 @@
+package co.develhope.springrepo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRepo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRepo1Application.class, args);
+	}
+
+}
